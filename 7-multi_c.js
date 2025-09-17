bca9bc5d885309bdce355861c2551c6ec45bcf5b
Kabelo Ramolotja <kr14rams@gmail.com>
2025-09-17 07:44:12 +0200

    added multi c file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const x = parseInt(ProcessingInstruction.argv[2]);
 if (isNaN(x)) {
-    console.log("Usage: node 7-multi_c.js 2");
+    console.log("Usage: node 7-multi_c.js 6");
 } else {
     for (let i = 0; i < x; i++) {
         console.log("C is fun");        
